1ebcc747e95b24a47649686a1298642e4569f8f3
Nicolas Salhuana <nicolas.salhuana@gmail.com>
2024-02-03 17:17:39 -0700

    tailwind and linter

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="text-center">
+      <header className="bg-slate-700 min-h-lvh flex flex-col items-center justify-center text-white text-2xl">
+        <img src={logo} className="h-[40vmin] pointer-events-none" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-cyan-400 underline"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React from Nic
         </a>
+        <h1 className="text-3xl font-bold underline bg-red-400">Hello world</h1>
       </header>
     </div>
   );
